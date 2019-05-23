c1d87091a98c74270d22d3af0720dcd189e35173
Gladleyfucker <jasatomicanin@yahoo.com>
2019-05-23 13:49:54 +0200

    DoTween + LeanTouch + TinderScene

---
+++ Port2019/Assets/Scripts/TinderTouch.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Lean.Touch;
+using System;
+using DG.Tweening;
+
+public class TinderTouch : MonoBehaviour
+{
+
+    public List<Transform> Characters = new List<Transform>();
+
+
+
+    public Transform mainImage;
+
+    public float XThreshold;
+
+    public float XMaxMove;
+    public float screenDivider;
+    public float ZMaxRotate;
+
+    private float yPivot = 0f;
+
+    private Vector2 startPos;
+    private Vector2 currPos = new Vector2(Screen.width/2,0f);
+    private Vector2 deltaMove;
+
+    private int fingerCount = 0;
+    private float tempX;
+    public float speedTween;
+    private float tempTime;
+
+    private void Awake()
+    {
+        yPivot = mainImage.localPosition.y;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        LeanTouch.OnFingerTap += OnFingerTap;
+        LeanTouch.OnGesture += OnSwipe;
+        LeanTouch.OnFingerExpired += OnFingerUp;
+
+        //XMaxMove = Screen.width / screenDivider;
+    }
+
+    private void OnFingerUp(LeanFinger obj)
+    {
+        fingerCount = 0;
+        OnFingerSwipe();
+
+        print("finger up!!!");
+    }
+
+    private void OnSwipe(List<LeanFinger> obj)
+    {
+        if (obj.Count == 1)
+        {
+            deltaMove = obj[0].ScreenPosition - startPos;
+            currPos =  obj[0].ScreenPosition ;
+            currPos = new Vector2(currPos.x - Screen.width / 2, currPos.y);
+            OnFingerSwipe();
+            //print(deltaMove);
+        }
+        else if (obj.Count == 0)
+        {
+            //currPos = Vector2.zero;
+        }
+
+        fingerCount = obj.Count;
+    }
+
+    private void OnFingerSwipe()
+    {
+        float x = currPos.x / XMaxMove;
+        tempX = 0f;
+        float rotateTemp = 0f;
+        //float rotateTem = 0f;
+        float currX = currPos.x;
+        if (currX > XMaxMove)
+        {
+            x = XMaxMove;
+            Match();
+        }
+        else if (currX < -XMaxMove)
+        {
+
+            x = -XMaxMove;
+            Next();
+        }
+        else
+        {
+            x = currX;
+        }
+        if (fingerCount == 0)
+        {
+            print("It clears!");
+            x = 0;
+        }
+        rotateTemp = x / XMaxMove;
+        rotateTemp *= ZMaxRotate;
+        //rotateTem = Mathf.Lerp(rotateTem, rotateTemp, strafeSpeed);
+        DOTween.Kill(mainImage);
+        //tempTime = Mathf.Abs(mainImage.eulerAngles.z - rotateTemp);
+        mainImage.DORotate(new Vector3(0f, 0f, rotateTemp),speedTween);
+        //mainImage.DOMoveX(x, speedTween);
+        //tempX = Mathf.Lerp(tempX, x, Time.deltaTime * strafeSpeed);
+        //mainImage.transform.eulerAngles = new Vector3(0f, 0f, rotateTem);
+    }
+
+    private void Match()
+    {
+
+    }
+
+    private void Next()
+    {
+
+    }
+
+    private void OnFingerTap(LeanFinger obj)
+    {
+        startPos = obj.StartScreenPosition;
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void LateUpdate()
+    {
+        //mainImage.rectTransform.localPosition = new Vector2( tempX,yPivot);
+        
+    }
+
+    IEnumerator RotateCour() 
+    {
+        return null;
+    }
+
+}
