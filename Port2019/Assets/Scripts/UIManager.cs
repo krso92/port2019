5d0dd96e98362e991e92b07e9b97926b80b35626
krso92 <srkyman@gmail.com>
2019-05-23 18:19:14 +0200

    Canvas added, ui manager, profile pic loaded

---
+++ Port2019/Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image profileImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("????");
+        profileImage.sprite = DateManager.Instance.currentDate.ProfileImage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
