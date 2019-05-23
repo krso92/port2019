8033e2a6b5200f2e641bc4f51bd9cb7aea54d091
Gladleyfucker <jasatomicanin@yahoo.com>
2019-05-23 10:42:02 +0200

    Singleton + Audio manager init.

---
+++ Port2019/Assets/Scripts/TGlobalSingleton.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class TGlobalSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    private static object _lock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            if (applicationIsQuitting)
+            {
+                return null;
+            }
+
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = (T)FindObjectOfType(typeof(T));
+
+                    if (FindObjectsOfType(typeof(T)).Length > 1)
+                    {
+                        Debug.LogError("[Singleton] Something went really wrong - there should never be more than 1 singleton! Reopening the scene might fix it.");
+                        return _instance;
+                    }
+
+                    if (_instance == null)
+                    {
+                        GameObject singleton = new GameObject();
+                        _instance = singleton.AddComponent<T>();
+                        singleton.name = "(singleton) " + typeof(T).ToString();
+
+                        DontDestroyOnLoad(singleton);
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+    private static bool applicationIsQuitting = false;
+
+    public void OnDestroy()
+    {
+        applicationIsQuitting = true;
+    }
+}
