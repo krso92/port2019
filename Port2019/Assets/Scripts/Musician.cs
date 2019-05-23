22f85a69d8321ea1148e6187164ad2e992e267cb
krso92 <srkyman@gmail.com>
2019-05-23 10:53:01 +0200

    Basic Musician class and relater controllers

---
+++ Port2019/Assets/Scripts/Musician.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musician : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip loopClip;
+    [SerializeField]
+    private float speed;
+
+    // component references
+    [Header("component references")]
+    [SerializeField]
+    private Animator animator;
+
+    // internal logic
+
+    private bool isPlaying;
+
+    // unity messages
+
+    protected void Start()
+    {
+        
+    }
+
+    protected void OnMouseDown()
+    {
+        // TODO -- efektic neki mini, cisto da se vidi, polu osvetljenje?
+    }
+
+    protected void OnMouseUp()
+    {
+        // TODO -- skloniti taj mini efektitj
+    }
+
+    protected void OnMouseUpAsButton()
+    {
+        // invoke event
+
+        // start music
+        // change animation
+        // 
+        Debug.Log("Musician starts playing");
+        isPlaying = !isPlaying;
+        // pokreni animacije znajuci ovo
+    }
+}
