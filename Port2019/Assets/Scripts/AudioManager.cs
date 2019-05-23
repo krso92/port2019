c2f54ec7264ae2a6fa4b74f2d416901c93526a75
Gladleyfucker <jasatomicanin@yahoo.com>
2019-05-23 11:09:44 +0200

    Audio man shit.

--- Port2019/Assets/Scripts/AudioManager.cs
+++ Port2019/Assets/Scripts/AudioManager.cs
@@ -7,7 +7,7 @@ public class AudioManager : TGlobalSingleton<AudioManager>
 {
 
 
-    AudioMixer masterMixer;
+    public AudioMixer masterMixer;
 
 
     private AudioMixerGroup effectsGroup;
@@ -20,16 +20,46 @@ public class AudioManager : TGlobalSingleton<AudioManager>
     public float effectsVolume = 0.8f;
     public float ambientVolume = 0.8f;
 
+    private List<AudioSource> audioSources = new List<AudioSource>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            audioSources.Add(gameObject.AddComponent<AudioSource>());
+            audioSources[i].outputAudioMixerGroup = musicGroup;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlayMusicAudio(AudioClip clip)
+    {
+        //masterMixer.
+    }
+
+    public void PlayMusicAudio(AudioClip clip,float time)
+    {
+        //masterMixer.
+    }
+
+
+    private AudioSource GetEmptyAudioSource()
+    {
+        for (int i = 0; i < audioSources.Count; i++)
+        {
+            if (audioSources[i].clip == null)
+            {
+                return audioSources[i];
+            }
+        }
+
+        Debug.LogError("There is not enough audio sources for all sounds!!!");
+        return null;
     }
 }
