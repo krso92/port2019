5d3f78db139fd831c53510a06b49636e9935903f
Gladleyfucker <jasatomicanin@yahoo.com>
2019-05-24 13:43:08 +0200

    Animations to.

---
+++ Port2019/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RotateAround : MonoBehaviour
+{
+
+    public Transform objectt;
+    public bool loop;
+    public float RotateAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
